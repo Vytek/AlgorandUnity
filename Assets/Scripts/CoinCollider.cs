ada26ae986f9244d335b63ccd042dde2a668e2c3
Enrico Speranza <enrico.speranza@gmail.com>
2021-04-01 17:18:25 +0200

    Update CoinCollider.cs

--- Assets/Scripts/CoinCollider.cs
+++ Assets/Scripts/CoinCollider.cs
@@ -33,21 +33,6 @@ public class CoinCollider : MonoBehaviour
     {
         GameObject otherObj = collider.gameObject;
         Debug.Log("Triggered with: " + otherObj);
-        //Star Algorand Transaction
-        //AlgorandManager.Instance.PayPlayerWithAlgorand();
-        //USING DISPATCHER
-        //UnityMainThreadDispatcher.Instance().Enqueue(AlgorandManager.Instance.PlayerWithAlgorandLoopCoroutine()); //VERY GOOD!
-        //UnityMainThreadDispatcher.Instance().EnqueueAsync(() => AlgorandManager.Instance.PayPlayerwithAlgorandFunction()); //NOT GOOD!
-        //UnityMainThreadDispatcher.Instance().EnqueueAsync(() => AlgorandManager.Instance.PayPlayerWithAlgorand()); //NOT GOOD!
-        //Dispatcher.InvokeAsync(() => AlgorandManager.Instance.PayPlayerwithAlgorandFunction()); //TRY?
-        //Dispatcher.InvokeAsync(() => AlgorandManager.Instance.PayPlayerWithAlgorand()); //TRY?
-        /* TEST USING UNIRX
-        RxDispatcher.Instance.Enqueue(() =>
-        {
-            AlgorandManager.Instance.PayPlayerwithAlgorandFunction();
-        });
-        */
-
         //Using BackgroundTasksProcessor
         Processor.Process(
             () =>
